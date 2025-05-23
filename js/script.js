4d04200306e2519cf0fee66f38fb967b1311badb
KarinaJ137 <k6513291@gmail.com>
2025-05-23 13:48:18 +0500

    siple prog

--- js/script.js
+++ js/script.js
@@ -5,3 +5,36 @@ b = 7;
 mul = a * b;
 console.log(mul);
 }
+
+{
+    // let photoInFolder, photoOnPage, numberOfPage;
+    // photoInFolder = parseInt(prompt("Введите кол-во фото в папке", 10));
+    // photoOnPage = parseInt(prompt("Введите кол-во фото на одной странице", 10));
+    // numberOfPage = Math.ceil(photoInFolder / photoOnPage); //округление
+    // alert("кол-во страниц = " + numberOfPage);
+}
+
+{
+    let a = 1, b;
+    b = a++; //увеличивает на единицу, инкремент
+    console.log(a, b);
+}
+
+{
+    // //Дано 4-значное число найти все его цифры
+    // let a = 15 % 4; //как пример 15 - яблоки, 4 - человека, остаток деления 3
+    // let b = Math.floor(14 % 5);
+    // console.log(a, b);
+
+    // let number, d1, d2, d3, d4;
+    // number = parseInt(prompt("Введите 4-значное число"));
+    // d1 = number % 10;
+    // d2 = Math.floor(number/10) % 10;
+    // d3 = Math.floor(number/100) % 10;
+    // d4 = Math.floor(number/1000) % 10;
+    // console.log(d1, d2, d3, d4);
+}
+
+{
+    
+}
