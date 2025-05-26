2f1da0fd0b9275566e32297a368dcdae00b707d5
KarinaJ137 <k6513291@gmail.com>
2025-05-26 15:17:58 +0500

    task DOM

--- js/taskDOM.js
+++ js/taskDOM.js
@@ -1,3 +1,4 @@
+//Задание 1
 const p = document.querySelector(".test-text");
 p.innerHTML = "Привет, <strong>мир!</strong> Меня зовут <i>Карина</i>."
 p.style.background = "lightblue";
@@ -5,3 +6,4 @@ p.style.color = "blue";
 p.style.border = "2px blue solid";
 console.log(p);
 
+//Задание 2
